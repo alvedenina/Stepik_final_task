58e441ba055c63c825c41b0bc4beab41fe2aa579
Alena <alvedenina@yandex.ru>
2020-04-03 17:47:30 +0300

    добавлена обработка исключений

--- Pages/base_page.py
+++ Pages/base_page.py
@@ -1,3 +1,5 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage():  #создаем класс
     def __init__(self, browser, url):     #добавляем метод, добавляем конструктор ключевое слово "__init__"
         self.browser = browser
@@ -5,3 +7,15 @@ class BasePage():  #создаем класс
 
     def open(self):                    #добавляем метод
         self.browser.get(self.url)
+
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
